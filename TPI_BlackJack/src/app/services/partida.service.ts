9e11a4f8f44faa60eea7342ed413ab9adc1e1f21
Alvarobasbus-112823 <69318172+Alvarobasbus@users.noreply.github.com>
2022-10-04 22:41:54 -0300

    cambios v3

--- TPI_BlackJack/src/app/services/partida.service.ts
+++ TPI_BlackJack/src/app/services/partida.service.ts
@@ -7,34 +7,35 @@ import { Carta } from '../models/carta';
 export class PartidaService {
 
   carta: Carta[] = [];
- 
+
 
   //palo = [0, 1, 2, 3];
   palo = ["c", "d", "t", "p"];
   numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
   indice: number;
   direc: string;
+  suma: number = 0;
 
 
   constructor() { }
 
-  barajar(){
-    
+  barajar() {
+
 
 
     for (let i = 0; i < this.palo.length; i++) {
       for (let j = 0; j < this.numero.length; j++) {
-        console.log(this.palo[i], this.numero[j])
 
         var obj = {
           numero: this.numero[j],
           palo: this.palo[i],
           activo: false,
-          direc: `../../assets/media/cartas/${this.numero[j]}${this.palo[i]}.png`
+          direc: `../../assets/media/cartas/${this.numero[j]}${this.palo[i]}.png`,
+          jugador: 0
         };
 
         this.carta.push(obj)
-        
+
 
       }
 
@@ -43,13 +44,35 @@ export class PartidaService {
 
   }
 
-  pedir(): Carta{
+  pedir(): Carta {
 
-    this.indice= Math.floor(Math.random() * 52 );
-    this.carta[this.indice].activo=true;
+    this.indice = Math.floor(Math.random() * 52);
     
+    if(this.carta[this.indice].activo==true){
+      console.log('la carta ya fue sacada, no se puede repetir.')
+      this.pedir();
+    }
+    this.carta[this.indice].activo = true;
+
     return this.carta[this.indice];
 
+  }
+
+  calcularPuntos(numero: number): number {
+
+    if (numero >= 10) {
+      this.suma += 10
+
+    }
+    if (numero < 10) {
+      this.suma += numero;
+    }
+
+    return this.suma;
+  }
+
+  reset(){
+    this.suma=0;
   }
 
 
