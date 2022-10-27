4a472fffaf44aecdf55a0b521dbe26fb366acaab
dperez112858 <112858@tecnicatura.frc.utn.edu.ar>
2022-10-27 18:22:41 -0300

    partidaservice

--- TPI_BlackJack/src/app/services/partida.service.ts
+++ TPI_BlackJack/src/app/services/partida.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Carta } from '../models/carta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PartidaService {
+
+  apiUrlBase: string = environment.baseUrl;
 
   carta: Carta[] = [];
 
@@ -17,7 +21,7 @@ export class PartidaService {
   suma: number = 0;
 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   barajar() {
     for (let i = 0; i < this.palo.length; i++) {
@@ -39,7 +43,7 @@ export class PartidaService {
 
   pedir(): Carta {
     this.indice = Math.floor(Math.random() * 52);
-    if(this.carta[this.indice].activo==true){
+    if (this.carta[this.indice].activo == true) {
       this.pedir();
     }
     this.carta[this.indice].activo = true;
@@ -61,12 +65,37 @@ export class PartidaService {
     return this.suma;
   }
 
-  reset(){
-    this.suma=0;
-    for(let i=0;i<52;i++){
-      this.carta[i].activo=false
+  reset() {
+    this.suma = 0;
+    for (let i = 0; i < 52; i++) {
+      this.carta[i].activo = false
     }
   }
+
+  //[GET]/puntos/{num}
+  obtenerPuntos(num: number): Observable<any> {
+    return this.http.get(this.apiUrlBase + "/puntos/" + num);
+  }
+
+  //[PUT]/as
+  actualizarPorAs(n: number, m: number): Observable<any> {
+    const url = this.apiUrlBase + "/as"
+    const headers = { 'content-type': 'application/json' };
+    const body = JSON.stringify(m);
+    return this.http.put(url, body, { 'headers': headers })
+  }
+
+  //[GET]/pedir/{num}
+  pedirCartaPorJugador(num: number): Observable<any> {
+    return this.http.get<Carta>(this.apiUrlBase + "/pedir/" + num);
+  }
+
+  //[GET]/pedir
+  pedirCarta(): Observable<any> {
+    return this.http.get(this.apiUrlBase + "/pedir");
+  }
+
+
 
 
 }
